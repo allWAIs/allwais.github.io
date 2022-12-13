99f21a2f9978165f20953499e2b0ea1be0fbc87c
codevil <namhundred@naver.com>
2022-12-13 20:58:19 +0900

    feat: add create issue
    페이지에서 직접 이슈를 작성할 수 있도록 기능 추가

--- src/pages/OpenIssue.tsx
+++ src/pages/OpenIssue.tsx
@@ -1,45 +1,37 @@
 import { useRef } from 'react';
 import { ContentsContainer } from '../components';
 import { Octokit } from '@octokit/rest';
+import { createAppAuth } from '@octokit/auth-app';
+
 const octokit = new Octokit({
-  auth: 'l4i2WmqZAVPaEbwgmFybuDwBGS3KtjHrgN+E56mvBiA=',
+  authStrategy: createAppAuth,
+  auth: {
+    appId: process.env.REACT_APP_APP_ID,
+    privateKey: process.env.REACT_APP_PRIVATE_KEY,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    installationId: process.env.REACT_APP_INSTALLATION_ID,
+  },
 });
+
 export function OpenIssue() {
   const issueTitle = useRef<HTMLInputElement | null>(null);
   const issueDetail = useRef<HTMLTextAreaElement | null>(null);
 
   const sendIssue = async (title?: string, detail?: string) => {
-    if (typeof title && typeof detail) {
-      octokit.rest.issues.create({
-        owner: 'allWAIs',
-        repo: 'allWAIs',
-        title: title + '',
-        body: detail,
-      });
-      // await octokit.request('POST /repos/{owner}/{repo}/issues', {
-      //   owner: 'allWAIs',
-      //   repo: 'allWAIs',
-      //   title: title + '',
-      //   body: detail,
-      //   // assignees: ['octocat'],
-      //   milestone: 1,
-      //   // labels: ['bug'],
-      // });
+    try {
+      if (typeof title && typeof detail) {
+        console.log(process.env);
+        await octokit.rest.issues.create({
+          owner: 'allWAIs',
+          repo: 'allWAIs',
+          title: title + '',
+          body: detail,
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    // fetch('https://api.github.com/repos/allWAIs/issues', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authorization: 'token ' + '3e92c76fb5dc5cba9f4d1080b7d108e69a381f7d',
-    //   },
-    //   body: JSON.stringify({
-    //     title: title,
-    //     body: detail,
-    //   }),
-    // }).then(() => {
-    //   // input 초기화 하기
-    // });
   };
   return (
     <>
